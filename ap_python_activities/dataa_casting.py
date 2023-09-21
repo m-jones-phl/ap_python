e45f11eb8e3ba337e64a5e0313c48094fb19479b
malik jones <145585837+m-jones-phl@users.noreply.github.com>
2023-09-21 15:28:01 +0000

    CLASSWORK

---
+++ ap_python_activities/dataa_casting.py
@@ -0,0 +1,21 @@
+# convert the following datatypes into floats.
+12.09
+100.87
+
+# convert the following floats into integers.
+(12.34)
+23.33
+
+
+# modify the code below to print an integer number. 
+num_one = 24.56
+num_two= 23.45
+
+print(num_one + num_two)
+
+# modify the following code below to work
+nameOfPet = "Spuny"
+ageOfPet = 10
+
+print("this is my dog "+ nameOfPet + ". He is " + ageOfPet + " years old.")
+ 
