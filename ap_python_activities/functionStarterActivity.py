6e40186923694efaa025142e13e3bdac5ede32e7
malik jones <145585837+m-jones-phl@users.noreply.github.com>
2023-09-22 13:36:49 -0400

    Update functionStarterActivity.py

--- ap_python_activities/functionStarterActivity.py
+++ ap_python_activities/functionStarterActivity.py
@@ -1,18 +1,32 @@
 # 1. In your own words, describe what a function is
+# allows you to store code
 
 # 2. What is are function parameters and arguments and describe
+# A parameter is a variable in a function
+
+
 # the difference between the 2. 
+# one is a placeholder one is a function
+
 
 # 3. write a function that will print out a welcome message
 # that includes a users name. You will need to use parameters and arguments
+Print("hello malik!")
+
 
 # 4. Write a function that will do a calculation that takes 3 parameters.
 # Your function can do any of the arithmatic operators (add, subtract, multiply, divide)
+def money = 20.00
+print (money)
+
 
 # 5. Write a function that will output a message to a user, telling them
 # what class they have next after this one. this code should use a 
 # variable to pass a value into the parameter of a function. The variable should
 # be real, user data- not hard coded data.
+
+
+Print( you have biology now)
 
 
 
