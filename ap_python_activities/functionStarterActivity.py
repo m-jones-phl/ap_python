745d073b7bb9637acdf47e223ab6bb63c85c5d7a
malik jones <145585837+m-jones-phl@users.noreply.github.com>
2023-09-26 10:12:22 -0400

    Update functionStarterActivity.py

--- ap_python_activities/functionStarterActivity.py
+++ ap_python_activities/functionStarterActivity.py
@@ -1,34 +1,45 @@
 # 1. In your own words, describe what a function is
-# allows you to store code
-
+peices of code you can reuse /  reuse instructions 
 # 2. What is are function parameters and arguments and describe
-# A parameter is a variable in a function
-
-
-# the difference between the 2. 
-# one is a placeholder one is a function
-
-
+# the difference between the 2.
+A parameter is a variable in a function. It is a placeholder. 
+An argument is a value passed during function invocation. 
 # 3. write a function that will print out a welcome message
 # that includes a users name. You will need to use parameters and arguments
-Print("hello malik!")
-
-
+user_name = input("Enter your name:-")
+print("Welcome to school,",jeff,"\b!")
 # 4. Write a function that will do a calculation that takes 3 parameters.
 # Your function can do any of the arithmatic operators (add, subtract, multiply, divide)
-def money = 20.00
-print (money)
+def addition():
+if string == "+":
+    a = num10 + num20
+    print("addition was performed on the two numbers ", num10, ' and ', num20)
+    return a
 
+def subtraction():
+if string == "-":
+    s = num1 - num2
+    print("subtraction was performed on the two numbers ", num1, ' and ', num2)
+    return s
+
+def multiplication():
+if string == "*":
+    t = num20 * num50
+    print("multiplication was performed on the two numbers ", num20, ' and ', num50)
+    return t
+
+def division():
+if string == "/":
+    d = num1 / num2
+    print("division was performed on the two numbers ", num1, ' and ', num2)
+    return d
 
 # 5. Write a function that will output a message to a user, telling them
 # what class they have next after this one. this code should use a 
 # variable to pass a value into the parameter of a function. The variable should
 # be real, user data- not hard coded data.
 
+def getClass():
+   name = "biology"
+   return name
 
-Print('you have biology now')
-
-
-
-
-# malik jones
