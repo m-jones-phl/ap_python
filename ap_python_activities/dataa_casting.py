4ea7852ca64373eb6f4f381bed9fb7bb31e8468f
malik jones <145585837+m-jones-phl@users.noreply.github.com>
2023-09-26 10:17:54 -0400

    Update dataa_casting.py

--- ap_python_activities/dataa_casting.py
+++ ap_python_activities/dataa_casting.py
@@ -1,21 +1,31 @@
 # convert the following datatypes into floats.
-12.09
-100.87
+12
+100
+
+a = float(12)
+b = float(100)
+
+print(a)
+print(b)
+
+print(float(12))
+print(float(100))
 
 # convert the following floats into integers.
-(12.34)
+12.34
 23.33
 
+print(int(12.34))
+print(int(23.33))
 
 # modify the code below to print an integer number. 
-num_one = 24.56
+num_one = 20
 num_two= 23.45
 
-print(num_one + num_two)
+print(int(num_one + num_two))
 
-# modify the following code below to work
-nameOfPet = "Spuny"
-ageOfPet = 10
+# modify the following code below to 
+name_of_pet = "Spuny"
+age_of_pet = 10
 
-print("this is my dog "+ nameOfPet + ". He is " + ageOfPet + " years old.")
- 
+print("this is my dog "+ name_of_pet + ". He is " + age_of_pet+ " years old.")
