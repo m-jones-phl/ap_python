b795a478e63e5cedc653ddf81e7a3fdf18ed0831
malik jones <145585837+m-jones-phl@users.noreply.github.com>
2023-09-27 15:27:59 +0000

    submit

---
+++ ap_python_activities/conditionalActivity.py
@@ -0,0 +1,45 @@
+# 1. What is the difference between 
+# a parameter and an argument in a python function
+
+#paramaters are the place holder in our functions definition
+
+# arguments are the actual data we pass into our function when we call it
+
+# 2. In your own words, describe what 
+# a conditional statement (if/else) is 
+
+# a tool in python that allow programs tocontroll the flow of program depending on the senario
+
+
+# 3. write a simple conditional state using a comparision operator(greater than, less than, etc. )
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+# 3. Write a conditional statement for food in the refridgerator.
+# your conditional statement should be wrapped in a function that takes one (1)
+# parameter. The data type for this parameter should be true or false. 
+# your function should have a line of code that asks the user if there is food in the refridgerator.
+# If there is food in the refridgerator, print time to cook. If there is 
+# NOT food in the fridge, print time to shop. 
+# When you call your function there should be an argument that accepts 
+# a boolean. 
+
+  a = food in the fridge
+b = no food in the fridge
+if b > a:
+  print("there is food in the fridge")
+
+
+# 4. Create a function that checks the inventory of cereal for a store. 
+# your function should parameter should accept an integer. In your function
+# use a conditional statement to determine if you need to order more cereal.
+# If there is more than 10 boxes, print "inventory full", else if there are less than 
+# 10 boxes print "we need to order more cereal".
+
+check inventory
+more than 10 boxes = full
+less than 10 boxes = "we need to order more cereal"
+if more than 10 boxes a:
+  print("b is greater than a")
