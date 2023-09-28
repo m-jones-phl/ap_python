ca6ca0326a3acd213189d411c48d1b1cecdf6c92
malik jones <145585837+m-jones-phl@users.noreply.github.com>
2023-09-28 11:26:56 -0400

    Update conditionalActivity2.py
    classwork

--- ap_python_activities/AP_Python_Class/apPythonActivities/conditionalActivity2.py
+++ ap_python_activities/AP_Python_Class/apPythonActivities/conditionalActivity2.py
@@ -8,6 +8,14 @@
 # "class is still in session."
 # your function should also alow the user to put in the time. The time should be 
 # formatted as a float. 
+  
+def ClassPeriod(2.00)
+    if Classperiod > 12.30
+    print('class is over LEAve .')
+else
+    print(' get ready for class')
+
+ClassPeriod()=
 
 # 2. Write a function that uses a conditional statement. 
 # your function should determine what type a pet a user has depeding on the data provided by the user
@@ -17,8 +25,15 @@
 # ELSE, if it is none of the animal sounds the function should return a message saying it doesn't 
 # know what the animal is. 
 
+def whatTypePet():
+  
+  if user(input(bark))
+   print('bark')
+
 # 3. Write a function that will take in a user name and height as parameters. 
 # Your function should evaluate and determine if the user is tall in enough to get on a roller coster.
 # IF the user is over 5.5, the function should return a custom message saying the user's name
 # and a message "welcome please buckle up".
 # ELSE if they they are not, return a message apologizing to the user saying they are not tall enough.
+def userNameandHeight()
+   if user is above 5.5
