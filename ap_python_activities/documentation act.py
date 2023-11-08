8f9f15659691bea4ff1f39950d7f3830d3b84812
malik jones <145585837+m-jones-phl@users.noreply.github.com>
2023-11-08 13:23:14 -0500

    Update documentation act.py
    i added heading

--- ap_python_activities/documentation act.py
+++ ap_python_activities/documentation act.py
@@ -1,11 +1,11 @@
-# One of the major project is th scan landscapes with of there drones to make a digital version of said
-# landscape with a website or newly introduced VR headset. T
+<h1>PROJ VERTSPACE<h1></h1>
+PUNCHMADE INC   /     https://github.com/m-jones-phl/ap_python/edit/main/ap_python_activities/documentation%20act.py
+The XR software could be use to predict how much work needs to be done on a piece of land
+1. One of the major project is th scan landscapes with of there drones to make a digital version of said
+# landscape with a website or newly introduced VR headset
+2. They take This data map can only be made through real world means 
+3. This data map can only be made through real world means
 
-# The XR software could be use to predict how much work needs to be done on a piece of land. Gaming engine
-# was created for game development or other complicated programs.
+**you can download this app om your phone , then use your cameara**
 
-# They take massive amounts of data and shrink it down to a program to use.
-# They have a 3d digital image of the world and this site can nearly go anywhere in the world. 
-
-# This data map can only be made through real world means, visualization with that being finish, there new goal is to upgrade
-# and improve upon their 3d program
+you can contribute to the program by scanning land , then uploading the scan to us via **@punchmadevii** on all platforms
