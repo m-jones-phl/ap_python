30fc59785bc1f28bdbce0a2d04bc5c03239ca219
malik jones <145585837+m-jones-phl@users.noreply.github.com>
2023-11-08 11:29:14 -0500

    monday Async Work.py
    my new work 11/6/23

---
+++ ap_python_activities/apPythonActivities.py/monday Async Work.py
@@ -0,0 +1,37 @@
+MONDAY ASYNC ACTIVITY
+
+Please visit and read the articles found on the links below:
+
+- Documentation
+https://www.writethedocs.org/guide/writing/beginners-guide-to-docs/#:~:text=Your%20first%20steps%20in%20documentation,will%20serve%20your%20project%20well.
+
+- Git Commit Messages
+https://dev.to/chrissiemhrk/git-commit-message-5e21
+
+When you are finished reading the articles, answer the following questions and commit and push 
+your work to your repository. 
+
+Your document should be named as followed async_Work_oct30.txt.
+
+This assignment is due by the end of class in order to get credit. 
+_______________________________________________________________________________
+
+1. List and explain three 3 reasons why programmers write documentation. 
+
+# Writing a documentation makes it easier to understand code 
+2. In your own words, describe what a README.md document is.
+
+#.to provide the proper extension
+  
+  #. It is also helps with interaction 
+3. Based on what your read, what are four (4) things that should be in a README document. 
+
+# BSD, MTT, GPL, Mailing list
+
+4. Name and describe three (3) reasons why programmers write commmit messages when using GitHub.
+
+# To save time
+
+5. Describe three things (3) negative outcomes that could happen if a programmer has bad or no documentation.
+
+# Without a good documentation the person or teammate reading your documentation might not know what is for , or what its about
